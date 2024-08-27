0ed0db75789bd0562e3b40171b93e5bdf82aabc2
MishaZorin <165201586+MishaZorin@users.noreply.github.com>
2024-08-27 19:31:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,31 +6,39 @@ let modal2 = document.getElementById('modal2')
 let galery = document.getElementById('galery')
 let galeryDesktop = document.getElementById('galeryDesktop')
 let galery_button = document.getElementById('galery_button')
-let about_us = document.getElementById('about_us')
-let contacts = document.getElementById('contacts')
-about_us.onclick = function () {
+let about_usDesktop = document.getElementById('about_usDesktop')
+let contactsDesktop = document.getElementById('contactsDesktop')
+about_usDesktop.onclick = function () {
     modal.style.display = 'block'
     modal1.style.display = 'none'
     modal2.style.display = 'none'
+    modal_home.style.display = 'none'
 
 }
-contacts.onclick = function () {
+contactsDesktop.onclick = function () {
+    console.log('r');
+    
     modal1.style.display = 'block'
     modal2.style.display = 'none'
     modal.style.display = 'none'
+    modal_home.style.display = 'none'
 }
-galery.onclick = function () {
-    modal2.style.display = 'block'
-     modal.style.display = 'none'
-      modal1.style.display = 'none'
-}
+
 galery_button.onclick = function () {
     modal2.style.display = 'block'
+    modal_home.style.display = 'none'
+    modal.style.display = 'none'
+    modal1.style.display = 'none'
 }
 galeryDesktop.onclick = function () {
     modal2.style.display = 'block'
     modal_home.style.display = 'none'
+    modal.style.display = 'none'
+    modal1.style.display = 'none'
 }
 home.onclick = function () {
+    modal.style.display = 'none'
+    modal1.style.display = 'none'
+    modal2.style.display = 'none'
     modal_home.style.display = 'block'
 }
